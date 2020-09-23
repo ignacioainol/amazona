e8e3a5d67890f203f4f2cf4ce04f13b7a0291eda
Ignacio <shoik.xtazy@gmail.com>
2020-09-22 23:22:04 -0300

    get data from api :D

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import data from './data'
 import HomeScreen from './Screens/HomeScreen'
 import ProductScreen from './Screens/ProductScreen'
 
