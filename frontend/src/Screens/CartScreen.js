82f782b14e9324ac9094e05c3afe7815bd8d4aa9
Ignacio <shoik.xtazy@gmail.com>
2020-09-26 20:21:37 -0300

    adding screen of cart

---
+++ frontend/src/Screens/CartScreen.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+function CartScreen(props) {
+    const productId = props.match.params.id;
+    const qry = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
+
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+
+    }, [])
+
+    return <div>Cart Screen</div>
+}
+
+export default CartScreen;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import thunk from 'redux-thunk';
+import { cartReducer } from './reducers/cartReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
 const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
