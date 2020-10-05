f833ace50b299d1c84a85f34cba29e6d0f002214
Ignacio <shoik.xtazy@gmail.com>
2020-10-05 13:22:30 -0300

    added cookies to save items in cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,8 +1,8 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
+import Cookie from 'js-cookie'
+import axios from 'axios'
 
-const axios = require("axios")
-
-const addToCart = (productId, qty) => async (dispatch) => {
+const addToCart = (productId, qty) => async (dispatch, getState) => {
     try {
         const { data } = await axios.get('/api/products/' + productId);
         dispatch({
@@ -15,13 +15,19 @@ const addToCart = (productId, qty) => async (dispatch) => {
                 qty
             }
         });
+
+        const { cart: { cartItems } } = getState();
+        Cookie.set("cartItems", JSON.stringify(cartItems));
     } catch (error) {
 
     }
 }
 
-const removeFromCart = (productId) => (dispatch) => {
+const removeFromCart = (productId) => (dispatch, getState) => {
     dispatch({ type: CART_REMOVE_ITEM, payload: productId })
+
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
 }
 
 export { addToCart, removeFromCart }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import thunk from 'redux-thunk';
+import Cookie from 'js-cookie'
 import { cartReducer } from './reducers/cartReducers';
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
