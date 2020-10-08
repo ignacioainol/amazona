f6585430322b64d4b0a9bbf283a3ae61735915de
Ignacio <shoik.xtazy@gmail.com>
2020-10-07 23:58:58 -0300

    finish connection to mongo step 1

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,1 +1,22 @@
 import express from 'express';
+import User from '../models/userModel';
+
+const router = express.Router();
+
+router.get("/api/users/createadmin", async (req, res) => {
+    try {
+        const user = new User({
+            name: 'Extazys',
+            email: "iggnaxios@gmail.com",
+            password: '123',
+            isAdmin: true
+        });
+
+        const newUser = await user.save();
+        res.send(user);
+    } catch (error) {
+        res.send({ msg: error.message });
+    }
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,21 @@ import data from './data';
 import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
 
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 
 mongoose.connect(mongodbUrl, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
 const app = express();
+
+app.use("/api/users", userRoute);
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
