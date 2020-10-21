268f78b396145d034a65f58df48a537bee7ac671
Ignacio <shoik.xtazy@gmail.com>
2020-10-21 00:33:14 -0300

    mmm copiado lo de github :c

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,6 +1,10 @@
+import Axios from 'axios';
 import axios from 'axios'
-const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
-    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL } = require("../constants/productConstants");
+
+import {
+    PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_SAVE_REQUEST, PRODUCT_SAVE_SUCCESS, PRODUCT_SAVE_FAIL, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_REQUEST
+} from "../constants/productConstants"
 
 
 
@@ -17,15 +21,25 @@ const listProducts = () => async (dispatch) => {
 const saveProduct = (product) => async (dispatch, getState) => {
     try {
         dispatch({ type: PRODUCT_SAVE_REQUEST, payload: product });
-        const { userSignIn: { userInfo } } = getState();
-        const { data } = await axios.post('/api/products', product, {
-            headers: {
-                'Authorization': 'Bearer ' + userInfo.token
-            }
-        });
-        dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+        const { userSignin: { userInfo } } = getState();
+        if (!product._id) {
+            const { data } = await Axios.post('/api/products', product, {
+                headers: {
+                    'Authorization': 'Bearer ' + userInfo.token
+                }
+            });
+            dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+        } else {
+            const { data } = await Axios.put('/api/products/' + product._id, product, {
+                headers: {
+                    'Authorization': 'Bearer ' + userInfo.token
+                }
+            });
+            dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
+        }
 
     } catch (error) {
+
         dispatch({ type: PRODUCT_SAVE_FAIL, payload: error.message });
     }
 }
@@ -40,4 +54,20 @@ const detailsProduct = (productId) => async (dispatch) => {
     }
 }
 
-export { listProducts, detailsProduct, saveProduct }
+const deleteProdcut = (productId) => async (dispatch, getState) => {
+    try {
+        const { userSignin: { userInfo } } = getState();
+        dispatch({ type: PRODUCT_DELETE_REQUEST, payload: productId });
+        const { data } = await axios.delete("/api/products/" + productId, {
+            headers: {
+                Authorization: 'Bearer ' + userInfo.token
+            }
+        });
+        dispatch({ type: PRODUCT_DELETE_SUCCESS, payload: data, success: true });
+    } catch (error) {
+        dispatch({ type: PRODUCT_DELETE_FAIL, payload: error.message });
+
+    }
+}
+
+export { listProducts, detailsProduct, saveProduct, deleteProdcut }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,9 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
-import { productDetailsReducer, productListReducer, productSaveReducer } from './reducers/productReducers';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import Cookie from 'js-cookie'
-import { cartReducer } from './reducers/cartReducers';
-import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
+import Cookie from 'js-cookie';
+import { productListReducer, productDetailsReducer, productSaveReducer, productDeleteReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers'
+import { userSigninReducer, userRegisterReducer } from './reducers/userReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
 const userInfo = Cookie.getJSON("userInfo") || null;
@@ -15,9 +15,9 @@ const reducer = combineReducers({
     cart: cartReducer,
     userSignin: userSigninReducer,
     userRegister: userRegisterReducer,
-    productSave: productSaveReducer
-});
-
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, compose(applyMiddleware(thunk)));
+    productSave: productSaveReducer,
+    productDelete: productDeleteReducer
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 export default store;
